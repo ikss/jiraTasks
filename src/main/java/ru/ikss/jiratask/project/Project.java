660aa6e9e685b700fbc4d04c4799b6d12e5edf20
KuznetsovS <s.kuznetsov@crystals.ru>
2016-11-15 15:21:56 +0300

    Правильное получение всех задач по запросу

--- src/main/java/ru/ikss/jiratask/project/Project.java
+++ src/main/java/ru/ikss/jiratask/project/Project.java
@@ -2,12 +2,16 @@ package ru.ikss.jiratask.project;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 
 import ru.ikss.jiratask.DAO;
@@ -15,20 +19,31 @@ import ru.ikss.jiratask.DAO;
 public abstract class Project {
 
     private static final Logger log = LoggerFactory.getLogger(Project.class);
+    private static final int MAX_RESULTS = 500;
+    private static final Set<String> fields =
+            Stream.of("summary", "issuetype", "created", "updated", "project", "status", "key").collect(Collectors.toSet());
 
     public abstract void handleTasks();
+
+    public abstract String getTimeQuery();
 
     public DateTime getLastTime() {
         return DAO.I.getTime(getTimeQuery());
     }
 
-    public abstract String getTimeQuery();
-
     public List<String> getAllTasks(JiraRestClient client, String query) {
         List<String> result = new ArrayList<>();
-        SearchResult searchResult = client.getSearchClient().searchJql(query).claim();
-        log.trace("Total works: {}", searchResult.getTotal());
-        searchResult.getIssues().forEach(i -> result.add(i.getKey()));
+        int startAt = 0;
+        boolean hasData = false;
+        do {
+            SearchResult searchResult = client.getSearchClient().searchJql(query, MAX_RESULTS, startAt, fields).claim();
+            for (Issue issue : searchResult.getIssues()) {
+                hasData = true;
+                result.add(issue.getKey());
+                startAt++;
+            }
+        } while (hasData);
+        log.trace("Total works: {}", result.size());
         return result;
     }
 
