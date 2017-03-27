fbd71df55bcb79e57686950c8cbd07bb474062e5
nenahov <nenahov@gmail.com>
2017-03-27 12:39:59 +0300

    Set10Project - add Summary

--- src/main/java/ru/ikss/jiratask/project/Set10Project.java
+++ src/main/java/ru/ikss/jiratask/project/Set10Project.java
@@ -32,7 +32,7 @@ import ru.ikss.jiratask.jira.JiraClient;
 public class Set10Project extends Project {
 
     private static final Logger log = LoggerFactory.getLogger(Set10Project.class);
-    private static final String INSERT_DATA = "select set10TaskInsert(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String INSERT_DATA = "select set10TaskInsert(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     private static final String INSERT_WORKLOG = "select set10WorkLogInsert(?, ?, ?, ?)";
     private static final String GET_TIME = "select set10GetLastTaskDate()";
     private static final String JQL = Config.getInstance().getValue("jira.jqlSet10");
@@ -105,6 +105,7 @@ public class Set10Project extends Project {
         st.setString(11, IssueHelper.getValueFromFieldByKey(issue, "parent", "key"));
         st.setString(12, IssueHelper.getValueFromFieldByKey(issue, "creator", "displayName"));
         st.setString(13, issue.getPriority().getName());
+        st.setString(14, issue.getSummary());
         log.debug("query: '{}'", st.toString());
         st.execute();
     }
